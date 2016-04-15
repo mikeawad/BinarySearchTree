7a19d3675ae83b1c51795c0d43a858255f5ea111
Mike Awad <awadbigmo@gmail.com>
2016-04-15 08:52:05 -0500

    Updated driver file

--- treetester.cpp
+++ treetester.cpp
@@ -15,9 +15,39 @@ int main()
    cout << "BST " << (intBST.empty() ? "is" : "is not") << " empty\n";
 
    // Testing inorder
-   cout << "Inorder Traversal of BST: \n";
+   cout << "Inorder Traversal of BST: \n\n\n";
    intBST.inorder(cout);
 
+
+   intBST.insert(500);
+
+   intBST.insert(750);
+   intBST.insert(250);
+
+   intBST.insert(125);
+   intBST.insert(375);
+
+   intBST.insert(625);
+   intBST.insert(875);
+
+   intBST.insert(120);
+   intBST.insert(130);
+
+   intBST.insert(370);
+   intBST.insert(380);
+/*
+   intBST.insert(630);
+   intBST.insert(620);
+
+   intBST.insert(870);
+   intBST.insert(880);
+   intBST.insert(835);
+
+*/
+
+
+
+/*
    // Testing insert
    cout << "\nNow insert a bunch of integers into the BST."
            "\nTry items not in the BST and some that are in it:\n";
@@ -29,7 +59,9 @@ int main()
       if (number == -999) break;
       intBST.insert(number);
    }
+*/
    intBST.graph(cout);
+   cout << endl << endl;
 
    cout << "BST " << (intBST.empty() ? "is" : "is not") << " empty\n";
    cout << "Inorder Traversal of BST: \n";
@@ -37,6 +69,10 @@ int main()
 
    cout << endl;
 
+   cout << endl << "Number of leaves = " << intBST.leafCount() << endl;
+   cout << endl << "Tree height = " << intBST.heightCount() << endl;
+
+/*
    // Testing search()
    cout << "\n\nNow testing the search() operation."
            "\nTry both items in the BST and some not in it:\n";
@@ -62,4 +98,6 @@ int main()
    cout << "\nInorder Traversal of BST: \n";
    intBST.inorder(cout);
    cout << endl;
+   */
+   return 0;
 }
